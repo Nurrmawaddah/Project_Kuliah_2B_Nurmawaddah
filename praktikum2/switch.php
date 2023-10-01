1eafe1162815f1676488626fbefa753c3e9633b3
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-01 14:19:11 +0700

    add file

--- praktikum2/switch.php
+++ praktikum2/switch.php
@@ -2,9 +2,15 @@
 <body>
     <?php $x=2; switch ($x) {
         case 1:
-            echo "Nomor 1"; break; case 2:
-            echo "Nomor 2"; break; case 3:
-            echo "Nomor 3"; break; case default:
+            echo "Nomor 1"; 
+            break; 
+        case 2:
+            echo "Nomor 2";
+            break; 
+        case 3:
+            echo "Nomor 3"; 
+            break; 
+        default:
             echo "Bukan Nomor diantara 1 dan 3";
     }
     ?>
