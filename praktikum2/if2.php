1d80b633c94f64b39fff3d1be28caaa2e9f46cc9
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-01 14:18:58 +0700

    add file

--- praktikum2/if2.php
+++ praktikum2/if2.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <body>
     <?php
-    $d=date("D"); if ($d=="Sat")
-    echo "Selamat berakhir pekan!";
+    $d=date("D");
+    if ($d=="Sat")
+        echo "Selamat berakhir pekan!";
     elseif ($d=="Fri")
-    echo "Selamat Menunaikan Sholat Jum'at bagi yang muslim!"; else
-    echo "Selamat belajar!";
+        echo "Selamat Menunaikan Sholat Jum'at bagi yang muslim!"; 
+    else
+        echo "Selamat belajar!";
     ?>
 </body>
 </html>
