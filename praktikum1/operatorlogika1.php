7e1343cc766c2480aac88d7e2ae51463b5c6b9ac
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-01 11:58:11 +0700

    add

--- praktikum1/operatorlogika1.php
+++ praktikum1/operatorlogika1.php
@@ -1,22 +1,53 @@
-<?php
-    $b = 4!=4;
-    $c = 3 + 7 == 10;
-    
-    $a = ($b and $c);
-    echo "\$a = $a <BR>";
-    
-    $a = ($b or $c);
-    echo "\$a = $a <BR>";
-    
-    $a = ($b xor $c);
-    echo "\$a = $a <BR>";
-    
-    $a = (!$b or $c);
-    echo "\$a = $a <BR>";
-    
-    $a = $b && $c;
-    echo "\$a = $a <BR>";
-    
-    $a = $b || $c;
-    echo "\$a = $a <BR>";
-?>
+<html>
+    <body>
+        <?php
+        $val1 = 0;
+        $val2 = 0;
+        echo "Operasi $val1 and $val2 adalah =";
+        echo $val1 && $val2;
+        echo "<br>";
+        echo "Operasi $val1 or $val2 adalah =";
+        echo $val1 || $val2;
+        echo "<br>";
+        echo "Operasi $val1 xor $val2 adalah ="; // Perbaikan: Gunakan "xor" dalam huruf kecil
+        echo $val1 xor $val2;
+        echo "<br>";
+
+        $val1 = 0;
+        $val2 = 1;
+        echo "Operasi $val1 and $val2 adalah =";
+        echo $val1 && $val2;
+        echo "<br>";
+        echo "Operasi $val1 or $val2 adalah =";
+        echo $val1 || $val2;
+        echo "<br>";
+        echo "Operasi $val1 xor $val2 adalah ="; // Perbaikan: Gunakan "xor" dalam huruf kecil
+        echo $val1 xor $val2;
+        echo "<br>";
+
+        $val1 = 1;
+        $val2 = 0;
+        echo "Operasi $val1 and $val2 adalah =";
+        echo $val1 && $val2;
+        echo "<br>";
+        echo "Operasi $val1 or $val2 adalah =";
+        echo $val1 || $val2;
+        echo "<br>";
+        echo "Operasi $val1 xor $val2 adalah ="; // Perbaikan: Gunakan "xor" dalam huruf kecil
+        echo $val1 xor $val2;
+        echo "<br>";
+
+        $val1 = 1;
+        $val2 = 1;
+        echo "Operasi $val1 and $val2 adalah =";
+        echo $val1 && $val2;
+        echo "<br>";
+        echo "Operasi $val1 or $val2 adalah =";
+        echo $val1 || $val2;
+        echo "<br>";
+        echo "Operasi $val1 xor $val2 adalah ="; // Perbaikan: Gunakan "xor" dalam huruf kecil
+        echo $val1 xor $val2;
+        echo "<br>";
+        ?>
+    </body>
+</html>
