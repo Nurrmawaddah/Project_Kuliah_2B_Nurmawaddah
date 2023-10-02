d0c28004f6cf6625a19096ecf8c4c64f69c9905a
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-02 10:23:56 +0700

    add

---
+++ praktikum3/contoh2.php
@@ -0,0 +1,15 @@
+<?php
+    echo" Menampilkan Array tanpa menunjuk nomor indeks";
+    echo"<br>";
+    echo"<br>";
+    $nama[] = "Muhammad";
+    $nama[] = "Faruq";
+    $nama[] = "Zakiuddin";
+    echo "Hallo";
+    echo " Isi array \$anak[0] adalah $nama[0]";
+    echo "<br>";
+    echo " Isi array \$anak[1] adalah $nama[1]";
+    echo "<br>";
+    echo " Isi array \$anak[2] adalah $nama[2]";
+    echo "<br>";
+?>
