1f896f58a7851471517bb6e8d06896523474a56c
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-02 10:23:50 +0700

    add

---
+++ praktikum3/contoh1.php
@@ -0,0 +1,16 @@
+
+<?php
+    echo" Menampilkan Array dengan menunjuk nomor indeks";
+    echo"<br>";
+    echo"<br>";
+    $anak[0] = "Faruq";
+    $anak[1] = "Alya";
+    $anak[2] = "Zahro";
+
+    echo"Isi array\$anak[0] adalah $anak[0]";
+    echo "<br>";
+    echo "Isi array \$anak[1] adalah $anak[1]";
+    echo"<br>";
+    echo "Isi array \$anak[2] adalah $anak[2]";
+    echo"<br>";
+?>
