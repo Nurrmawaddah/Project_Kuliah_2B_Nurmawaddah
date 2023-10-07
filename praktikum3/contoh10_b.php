4752149017ad65b43b0f2e65aa1126f477801da3
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-07 20:35:03 +0700

    update file

---
+++ praktikum3/contoh10_b.php
@@ -0,0 +1,7 @@
+<?php 
+    require ("contoh_requeire.php"); //Akan dipanggil 1x saja
+    //dalam file php ini
+    tulisantebal("Ini adalah tulisan tebal");
+    echo"<br>";
+    echo $a; //Mengambil nilai require
+?>
