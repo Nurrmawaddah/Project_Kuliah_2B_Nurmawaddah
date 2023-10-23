ca2851c9fca8916eb923ce2eb353be1d7e2a96fd
nurmawaddah <nurmawddh.net2022@gmail.com>
2023-10-23 14:04:47 +0700

    add file

---
+++ decafe/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Admin@gmail.comm
+                        </a>
+                        <ul class="dropdown-menu mt-2">
+                            <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle"></i> Profile</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="bi bi-key"></i> Ubah Password</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">LogOut</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ decafe/readme.md
@@ -0,0 +1,22 @@
+# Aplikasi Pemesanan Cafe/Restoran (DeCafe)
+Sebuah aplikasi yang dapat dingunakan untuk pemesanan minuman dan makanan pada cafe, restoran, rumah makan atau yang sejenisnya.
+
+## Pemilik/admin
+ -Melihat laporan
+ -Menambah/mengubah/menghapus daftar menu dan harga
+ -Menambah/mengubah/menghapus user
+## Kasir
+ -Melihat harga dan total harga pesananan
+ -Konfirmasi pembayaran
+ -Membuat bukti pembayaran
+## Pelayan
+ -Melihat menu dan harga
+ -Memilih menu
+ -Melihat total harga pesanan
+ -Membuat pesanan
+ -Update pesanan
+ -Hapus pesanan
+## Dapur
+ -Menerima pesanan
+ -Konfirmasi terima pesanan
+ -Mengubah status pesanan "siap saji"
